4b796b9d01e114432e21d252ddf7b25343984c47
= <=>
2021-11-24 06:16:10 +0000

    api 후statistics_card 수치 바뀌는 함수 완성

--- src/apis.py
+++ src/apis.py
@@ -2,6 +2,7 @@ import pymongo
 from flask import Blueprint, jsonify, request
 import concatParts as cp
 import concatNewses as cn
+import getSelectors as gs
 
 apis = Blueprint(__name__, 'apis')
 
@@ -12,27 +13,92 @@ def getParams():
 
     if selected_date == "total":
         is_date_total = True
+    else:
+        is_date_total = False
     if selected_keyword == "total":
         is_keyword_total = True
+    else:
+        is_keyword_total = False
     if selected_company == "total":
         is_company_total = True
+    else:
+        is_company_total = False
+
     return {
-        is_total: (is_date_total, is_keyword_total, is_company_total),
-        params: (selected_date, selected_keyword, selected_company)
+        "is_total": (is_date_total, is_keyword_total, is_company_total),
+        "params": (selected_date, selected_keyword, selected_company)
     }
 
 
 @apis.route('/api/statistics', methods=["GET"])
 def statistics():
-    reqested = getParams()
+    requested = getParams()
+    is_total = "is_total"
+    params = "params"
 
     if requested[is_total][0]:
         if requested[is_total][1]:
             if requested[is_total][2]:
-                return jsonify(cp.)
-
+                return jsonify(cp.date_total_keyword_total_company_total())
+            else:
+                return jsonify(cp.date_total_keyword_total_company_specific(requested[params][2]))
+        else:
+            if requested[is_total][2]:
+                return jsonify(cp.date_total_keyword_specific_company_total(requested[params][1]))
+            else:
+                return jsonify(cp.date_total_keyword_specific_company_specific(requested[params][1], requested[params][2]))
+    else:
+        if requested[is_total][1]:
+            if requested[is_total][2]:
+                return jsonify(cp.date_specific_keyword_total_company_total(requested[params][0]))
+            else:
+                return jsonify(cp.date_specific_keyword_total_company_specific(requested[params][0], requested[params][2]))
+        else:
+            if requested[is_total][2]:
+                return jsonify(cp.date_specific_keyword_specific_company_total(requested[params][0], requested[params][1]))
+            else:
+                return jsonify(cp.date_specific_keyword_specific_company_specific(requested[params][0], requested[params][1], requested[params][2]))
 
 
 
 @apis.route('/api/newses', methods=["GET"])
 def news_list():
+    requested = getParams()
+    is_total = "is_total"
+    params = "params"
+
+    if requested[is_total][0]:
+        if requested[is_total][1]:
+            if requested[is_total][2]:
+                return jsonify(cn.date_total_keyword_total_company_total())
+            else:
+                return jsonify(cn.date_total_keyword_total_company_specific(requested[params][2]))
+        else:
+            if requested[is_total][2]:
+                return jsonify(cn.date_total_keyword_specific_company_total(requested[params][1]))
+            else:
+                return jsonify(cn.date_total_keyword_specific_company_specific(requested[params][1], requested[params][2]))
+    else:
+        if requested[is_total][1]:
+            if requested[is_total][2]:
+                return jsonify(cn.date_specific_keyword_total_company_total(requested[params][0]))
+            else:
+                return jsonify(cn.date_specific_keyword_total_company_specific(requested[params][0], requested[params][2]))
+        else:
+            if requested[is_total][2]:
+                return jsonify(cn.date_specific_keyword_specific_company_total(requested[params][0], requested[params][1]))
+            else:
+                return jsonify(cn.date_specific_keyword_specific_company_specific(requested[params][0], requested[params][1], requested[params][2]))
+
+@apis.route('/api/selectors', methods=["GET"])
+def selectors():
+    selectors = gs.getSelectors()
+    result = {}
+    for key in selectors.keys():
+        temp = []
+        for selector in selectors[key].keys():
+            temp.append(selector)
+        result[key] = temp
+    return jsonify(result)
+
+
--- src/static/scripts2.js
+++ src/static/scripts2.js
@@ -2,16 +2,111 @@
 const addEventToId = (event, _id, callback) => {
     document.querySelector(`#${_id}`)?.addEventListener(event, callback)
 }
+
+let elemForSelectingDate = document.querySelector('#selectDate')
+let elemForSelectingKeyword = document.querySelector('#selectKeyword')
+let elemForSelectingCompany = document.querySelector('#selectCompany')
+
 const apiOrigin = "https://gamjeong.tk/api/"
-const getJsonFromApi = async (apiPathname, requestData) => {
-    let res = await fetch(apiOrigin + apiPathname, {
-        method: "GET",
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(requestData)
-    })
+const getJsonFromApi = async (apiPathname, data) => {
+    let res = await fetch(apiOrigin + apiPathname + new URLSearchParams(data))
     let responseJson = await res.json()
     return responseJson
 }
 
+const getSelectors = async () => {
+    let res = await fetch('/api/selectors')
+    let selectors = await res.json()
 
-document.querySelector('#selectForDate').addEventListener("change", () => console.log("changed"))
+    selectors.date.forEach(selector => {
+        let option = document.createElement('option')
+        option.innerText = selector
+        elemForSelectingDate.append(option)
+    })
+
+    selectors.category.forEach(selector => {
+        let option = document.createElement('option')
+        option.innerText = selector
+        elemForSelectingKeyword.append(option)
+    })
+
+    selectors.company.forEach(selector => {
+        let option = document.createElement('option')
+        option.innerText = selector
+        elemForSelectingCompany.append(option)
+    })
+}
+
+const changeStatisticsCardValue = async reqData => {
+    responseData = await getJsonFromApi("statistics?", reqData)
+    if ("statistics" in responseData){
+        data = responseData.statistics
+    } else {
+        data = responseData
+    }
+
+    let total = data.total
+    let positive =  data.positive
+    let negative =  data.negative
+    let normal =  data.normal
+    let infections = data.infections
+
+    document.querySelector('#statisticsCardTotal').innerText = total
+    document.querySelector('#infectionsCount').innerText = infections
+    document.querySelector('#statisticsCardPositive').innerText = `${Math.round(positive / total * 100)}%`
+    document.querySelector('#statisticsCardNegative').innerText = `${Math.round(negative / total * 100)}%`
+    document.querySelector('#statisticsCardNormal').innerText = `${Math.round(normal / total * 100)}%`
+
+    provideRedColorForHighest(positive, negative, normal)
+}
+
+const provideRedColorForHighest = (positive, negative, normal) => {
+    let highest = Math.max(positive, negative, normal)
+    if (positive === highest) {
+        document.querySelector('#statisticsCardPositive').style.color = 'red'
+        document.querySelector('#statisticsCardNegative').style.color = 'black'
+        document.querySelector('#statisticsCardNormal').style.color = 'black'
+    } else if (negative === highest) {
+        document.querySelector('#statisticsCardPositive').style.color = 'black'
+        document.querySelector('#statisticsCardNegative').style.color = 'red'
+        document.querySelector('#statisticsCardNormal').style.color = 'black'
+    } else {
+        document.querySelector('#statisticsCardPositive').style.color = 'black'
+        document.querySelector('#statisticsCardNegative').style.color = 'black'
+        document.querySelector('#statisticsCardNormal').style.color = 'red'
+    }
+}
+
+const createPieChartCanvas = async ()  => {
+    return `<canvas id="positiveNegativePieChart" ></canvas>`
+}
+
+const changeNewsChartContent = async reqData => {
+    return
+}
+
+
+const getNewData = async () => {
+    let date = elemForSelectingDate.value
+    let keyword = elemForSelectingKeyword.value
+    let company = elemForSelectingCompany.value
+
+    let data = {date, keyword, company}
+
+    changeStatisticsCardValue(data)
+
+    if (company === "total"){
+        document.querySelector('#divForPieChart').innerHTML = ""
+    } else {
+        document.querySelector('#divForPieChart').innerHTML = await createPieChartCanvas()
+        await drawPieChart(data)
+
+    }
+    changeNewsChartContent(data)
+}
+
+addEventToId("change", "selectDate", getNewData)
+addEventToId("change", "selectKeyword", getNewData)
+addEventToId("change", "selectCompany", getNewData)
+
+getSelectors()
